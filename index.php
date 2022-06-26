32f19eb946502f7e06ccca804d42ec6d2141e647
dhananjayvaidya <dhananjay.vaidya@icloud.com>
2022-06-26 14:29:05 +0530

    Toggle button

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="css/custom.css" />
+        <link rel="icon" type="image/x-icon" href="/img/lines-to-commas-favicon.png" />
+        <link rel="apple-touch-icon" href="/img/lines-to-commas-favicon.png" />
+        <meta name='description' content="Convert multiple lines to comma separated string | Convert multiple commas string to mutiple lines."/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     </head>
     <body>
         <div class="container">
@@ -20,18 +24,12 @@
                     <!--    </li>-->
                     <!--</ul>-->
                 </div>
-                <div class="col-md-4">
-                    <textarea class="input-text" placeholder="Enter your text" id="input-text"></textarea>
-                </div>
-                <div class="col-md-4">
-                    <div class="output-div" id="output"></div>
-                </div>
-                <div class="col-md-4">
+                <div class="col-md-12 mb-30">
                     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3724557629304393"
                     crossorigin="anonymous"></script>
                     <!-- LineToComma_1 -->
                     <ins class="adsbygoogle"
-                    style="display:block"
+                    style="display:block;width:100%;text-align:center;"
                     data-ad-client="ca-pub-3724557629304393"
                     data-ad-slot="1730760706"
                     data-ad-format="auto"
@@ -40,12 +38,22 @@
                     (adsbygoogle = window.adsbygoogle || []).push({});
                     </script>
                 </div>
+                <div class="col-md-5 ">
+                    <textarea class="input-text" placeholder="Enter your text to convert multi lines to comma separated string." id="input-text"></textarea>
+                </div>
+                <div class="col-md-2 align-center v-align-center">
+                    <img src='/img/two-arrows.png' class='toggle-btn' id='toggle-btn' />
+                </div>
+                <div class="col-md-5">
+                    <textarea class="output-div" id="output"></textarea>
+                </div>
+                
                 <div class="col-md-12 mt-30">
                     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3724557629304393"
                      crossorigin="anonymous"></script>
                     <!-- LineToComma_2 -->
                     <ins class="adsbygoogle"
-                     style="display:block"
+                     style="display:block,width:100%;"
                      data-ad-client="ca-pub-3724557629304393"
                      data-ad-slot="5654527999"
                      data-ad-format="auto"
@@ -53,6 +61,9 @@
                     <script>
                      (adsbygoogle = window.adsbygoogle || []).push({});
                     </script>
+                </div>
+                <div class="col-md-12 mt-30 text-center">
+                    Crafted with Love by <a href='https://dhananjayvaidya.com?ref=lines-to-comma' target="_blank">Dhananjay Vaidya</a>
                 </div>
             </div>
         </div>
@@ -67,9 +78,20 @@
             }else{
                 outputData = replaceCommaLine(inputData);
             }
-            $('#output').html(htmlencode(outputData));
+            $('#output').val(htmlencode(outputData));
         });
-            
+        $("#toggle-btn").click(function(){
+            action = (action == 1? 2:1);
+            inputData = $('#input-text').val()
+            if (action == 1){
+                outputData = replaceLineComma(inputData);
+                $('#input-text').attr('placeholder','Enter your text to convert multi lines to comma separated string.');
+            }else{
+                outputData = replaceCommaLine(inputData);
+                $("#input-text").attr('placeholder','Enter your text to convert comma separated string to multi lines string');
+            }
+            $('#output').val(htmlencode(outputData));
+        });
             function replaceLineComma(data) {
                 //convert string to array and remove whitespace
                 let dataToArray = data.split("\n").map(item => item.trim());
